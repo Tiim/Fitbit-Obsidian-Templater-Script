72f6eba96f1ea14cade460a3f214c96e65fbd9d1
Tiim <tim.bachmann96@gmail.com>
2022-04-21 21:58:01 +0200

    Update fitbit.js

--- fitbit.js
+++ fitbit.js
@@ -1,29 +1,9 @@
 const path = require("path");
 const fs = require("fs").promises;
 
-/*
-# Fitbit Activity in Obsidian
 
-## What does it do?
-
-Insert a list of your fitbit activities into an obsidian note.
-If you like this script, please consider [buying me a coffee](https://www.buymeacoffee.com/Tiim).
-
-## How to use:
-
-1. Put this script in your templater script directory.
-2. Edit the DIR variable below to point to a folder of your choice. 
-  This folder must exist. The file "fibit.json" will be created inside it.
-3. Create a templater template with the following content: 
-  `<% tp.user.fitbit(tp.file.creation_date()) %>`
-4. Run the templater script (by inserting the template to an existing file) to create the "fitbit.json" file. 
-  The error message is expected behavior and can be ignored.
-5. Go to [tiim.ch/fitbit-oauth2-token/](https://tiim.ch/fitbit-oauth2-token/) and follow the instructions 
-  to get your fitbit authentication code.
-6. Edit the file "fibit.json" and replace its content with the JSON document given in the step above.
-7. Run the templater script again, the file "fitbit.json" will be updated and your activities of the day 
-  will be inserted to the note.
-*/
+//Usage: see the README.md file.
+ 
 const DIR = path.join(app.vault.adapter.basePath, "misc");
 const FILE = path.join(DIR, "fitbit.json");
 const LANG = "de-CH";
